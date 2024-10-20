231f98ea8a92232c570be846baa22b969f9b33d4
Vlad4774 <vladbaracut7@yahoo.com>
2024-10-20 12:20:32 +0300

    Lab2

---
+++ Baracut_Vlad-Alexandru_Lab2/Baracut_Vlad-Alexandru_Lab2/Pages/Publishers/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Baracut_Vlad_Alexandru_Lab2.Data;
+using Baracut_Vlad_Alexandru_Lab2.Models;
+
+namespace Baracut_Vlad_Alexandru_Lab2.Pages.Publishers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context _context;
+
+        public DeleteModel(Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FindAsync(id);
+            if (publisher != null)
+            {
+                Publisher = publisher;
+                _context.Publisher.Remove(Publisher);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
