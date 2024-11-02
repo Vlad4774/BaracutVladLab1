96822f1574188fa861539ffdaf335c9e0777d210
Baracut Vlad <vladbaracut7@yahoo.com>
2024-11-02 16:52:12 +0200

    Update Again The files

---
+++ Baracut_Vlad-Alexandru_Lab2/Baracut_Vlad-Alexandru_Lab2/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Baracut_Vlad_Alexandru_Lab2.Data;
+using Baracut_Vlad_Alexandru_Lab2.Models;
+
+namespace Baracut_Vlad_Alexandru_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context _context;
+
+        public DetailsModel(Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Baracut_Vlad-Alexandru_Lab3/Baracut_Vlad-Alexandru_Lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Baracut_Vlad_Alexandru_Lab2.Data;
+using Baracut_Vlad_Alexandru_Lab2.Models;
+
+namespace Baracut_Vlad_Alexandru_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context _context;
+
+        public IndexModel(Baracut_Vlad_Alexandru_Lab2.Data.Baracut_Vlad_Alexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID)
+        {
+            BookD = new BookData();
+
+            //se va include Author conform cu sarcina de la lab 2
+            BookD.Books = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.BookCategories)
+            .ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .OrderBy(b => b.Title)
+            .ToListAsync();
+            if (id != null)
+            {
+                BookID = id.Value;
+                Book book = BookD.Books
+                .Where(i => i.ID == id.Value).Single();
+                BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
+        }
+    }
+}
